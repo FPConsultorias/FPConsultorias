9cb917fc9a23472bf4659d9b1e0c70c6c2533680
Fernando Pineda <contacto@fpconsultorias.it.com>
2025-07-06 16:36:52 -0500

    Eliminar archivos del runner local del repositorio

--- config.cmd
+++
@@ -1,3 +0,0 @@
-# Create the runner and start the configuration experience
-$ ./config.cmd --url https://github.com/FPConsultorias/FPConsultorias --token BUBZ7JOXZGS4QG5U6IGSX4DINL2IQ# Run it!
-$ ./run.cmd
